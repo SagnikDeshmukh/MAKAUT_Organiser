b9e30ed602e9c2b10d855fec90a33403205429bb
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2023-10-12 19:25:58 +0530

    dark theme

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ document.getElementById("moon").addEventListener("click", () => {
     // document.body.classList.toggle("dark");
     let icon = document.getElementById("moon");
     if(document.documentElement.classList.contains("dark")){
-        icon.src="../Resources/sun.svg"
+        icon.src="../Resources/moon.svg"
         document.documentElement.classList.remove("dark")
     }
     else{
-        icon.src="./Resources/moon.svg"
+        icon.src="./Resources/sun.svg"
         document.documentElement.classList.add("dark");
     }
     // window.location.reload();
 })
 
-if(localStorage.theme=="dark"){
-    document.documentElement.classList.remove("dark");
-}
-else document.documentElement.classList.remove("dark");
 
-localStorage.theme = "light";
+
