087487d1a3ac268b2cc145c111193822a1dd4fc8
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2023-10-13 19:21:03 +0530

    purge2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["**/*.css"],
+  content: ["./**/*.{css,html,js}"],
   options: {
     safelist: ['dark']
   },
