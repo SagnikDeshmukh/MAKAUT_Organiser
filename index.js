8f1dbfced65013b10a5ad5bb9a66411e7dabcc3c
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2023-10-13 20:47:57 +0530

    update in bin/vite

--- index.js
+++ index.js
@@ -48,17 +48,30 @@ loadingAnimation()
 
 // --------------------- Adding Dark Theme --------------------
 
+let icon = document.getElementById("moon");
+if(document.documentElement.classList.contains("dark")){
+    icon.src="./Resources/moon.svg"
+    document.documentElement.classList.remove("dark")
+}
+else{
+    icon.src="./Resources/sun.svg"
+    document.documentElement.classList.add("dark");
+}
+
+
+
 document.getElementById("moon").addEventListener("click", () => {
     // document.body.classList.toggle("dark");
     let icon = document.getElementById("moon");
-    if(document.documentElement.classList.contains("dark")){
-        icon.src="./Resources/moon.svg"
-        document.documentElement.classList.remove("dark")
-    }
-    else{
-        icon.src="./Resources/sun.svg"
-        document.documentElement.classList.add("dark");
-    }
+if(document.documentElement.classList.contains("dark")){
+    icon.src="./Resources/moon.svg"
+    document.documentElement.classList.remove("dark")
+}
+else{
+    icon.src="./Resources/sun.svg"
+    document.documentElement.classList.add("dark");
+}
+
     // window.location.reload();
 })
 
